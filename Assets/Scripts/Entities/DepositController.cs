10f05641ffcd266229c120396c6de2586dd98e77
shw0514 <gusdnddl50@gmail.com>
2024-02-07 10:35:50 +0900

    Revert "수정"
    This reverts commit cbf9ebe1373760b8e33418ba977f2bfdf94c312e.

--- Assets/Scripts/Entities/DepositController.cs
+++ Assets/Scripts/Entities/DepositController.cs
@@ -8,7 +8,6 @@ public class DepositController : MonoBehaviour
 {
     public InputField inputField;
     public int inputValue;
-    public bool isCount;
     // Start is called before the first frame update
     void Start()
     {
@@ -62,17 +61,13 @@ public class DepositController : MonoBehaviour
 
     public void DepositInputValue()
     {
-        isCount = int.TryParse(inputField.text, out int inputValue);
+        inputValue = int.Parse(inputField.text);
         if (DataManager.instance.ownedCashData >= inputValue)
         {
             DataManager.instance.ownedCashData -= inputValue;
             DataManager.instance.balanceCashData += inputValue;
         }
-        else if (!isCount)
-        {
-            Debug.Log("숫자를 입력해주세요");
-        }
-        else if (DataManager.instance.ownedCashData < inputValue)
+        else
         {
             DataManager.instance.OpenWarningMenu();
         }
